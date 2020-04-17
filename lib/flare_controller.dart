2fbe63987d63ab60b0604c2746655f9a2a2252bf
Bruno Glauco Eles <bgeles@gmail.com>
2020-04-17 16:36:52 -0300

    flare_controller

---
+++ lib/flare_controller.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_controller.dart';
+
+class AnimationControls extends FlareController {
+  void initialize(FlutterActorArtboard artboard){
+    
+  }
+
+  void setViewTransform(Mat2D viewTransform){}
+
+  bool advance(FlutterActorArtboard artboard, double elapsed){
+}
---
+++ lib/tracking_input.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controls.dart';
+import 'package:flare_flutter/flare_controller.dart';
+
+class TrackingInput extends StatefulWidget {
+  @override
+  _TrackingInputState createState() => _TrackingInputState();
+}
+
+class _TrackingInputState extends State<TrackingInput> {
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
